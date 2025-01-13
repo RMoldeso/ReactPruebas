60545ec7f4bda0eb611e41370e1949952e021295
Rodrigo Moldes <moldesrodrigor@gmail.com>
2025-01-12 22:28:12 -0400

    Practica1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import React from 'react';
 import './App.css'
 
 import Home from './screens/Home.jsx';
 import Counter from './Components/Counter/Counter.jsx';
+import FilterableProductTable from './Components/Home/FilterableProductTable.jsx';
+
+ const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
+];
 
 function App() {
   return (
     <>
         <Home />
         <Counter />
+        <FilterableProductTable products={PRODUCTS} />
     </>
   )
 }
 
 export default App;
+
+//<FilterableProductTable products={PRODUCTS} />
